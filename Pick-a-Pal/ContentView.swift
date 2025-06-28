cd0f338b3c10379a36755a9fb33172219b58f69c
Em Pongpat <empongpat@gmail.com>
2025-06-28 23:58:53 +0900

    feat: replace fixed of list TextView with State

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -8,13 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    
     var body: some View {
         VStack {
             List {
-                Text("Elisha")
-                Text("Andre")
-                Text("Jasmine")
-                Text("Po-Chun")
+                ForEach(names, id: \.description) { name in Text(name) }
             }
         }
         .padding()
