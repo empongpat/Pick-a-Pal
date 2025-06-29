e705f5849ac8645a311615cd3475d8017075cbcc
Em Pongpat <empongpat@gmail.com>
2025-06-29 23:43:52 +0900

    feat: adds TextField for the user to add name to the list

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    @State private var nameToAdd = ""
     
     var body: some View {
         VStack {
             List {
                 ForEach(names, id: \.description) { name in Text(name) }
             }
+            
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
         }
         .padding()
     }
